fce86db1deca63557593d4192e34ec893520a3da
Amit.manoshevitz <Amit.manoshevitz@post.runi.ac.il>
2024-01-01 19:05:04 +0200

    HW

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -24,6 +24,66 @@ public class OneOfEachStats {
 		//// just like you had in the previous version, except that the 
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
-		    
-	}
+    String kid = "";
+    int num = 1;
+    int x = 0;
+    int fam2 = 0;
+    int fam3 = 0;
+    int fam4 = 0;
+    int y = 0;
+
+
+while (x<T){
+    x++;
+    double ran1 = Math.random();
+    double ran2 = Math.random();
+    
+
+    /////////////////////////////
+    if (ran1 >= 0.5) {
+        kid = kid + "g";} 
+    else {kid = kid + "b";} 
+        
+    while ((ran1 < 0.5 && ran2 < 0.5) || (ran1 >= 0.5 && ran2 >= 0.5)){
+        if(ran2>=0.5){
+            kid = kid + " g";}
+        else{kid = kid + " b";}
+            ran2 = Math.random();
+            num++;}
+
+    if(ran2>=0.5){ 
+        kid = kid + " g";
+        num++;}
+    else{kid = kid + " b";
+        num++;}
+
+ ///////////////////////////////////       
+    if (num == 2) {
+        fam2++;
+    }
+    if (num == 3) {
+        fam3++;
+    }
+    if (num >= 4) {
+        fam4++;
+    }
+    
+    y = y + num;
+    num = 1;
+}
+double av = y/T;
+
+    System.out.println("Avrage: "+ av +" children to get at least one of each gender.");
+    System.out.println("Number of families with 2 children: "+fam2);
+    System.out.println("Number of families with 3 children: "+fam3);
+    System.out.println("Number of families with 4 or more children: "+fam4);
+    
+    if (fam2>fam3 && fam2>fam4){
+        System.out.println("The most common number of children is "+2+ ".");}
+    else if (fam3>fam2 && fam3>fam4){
+        System.out.println("The most common number of children is "+3+ ".");}
+    else{
+        System.out.println("The most common number of children is "+4+ " or more.");}
+    
+}
 }
